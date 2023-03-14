1d27ebaff59d98ff05266e9d50c97dff8a426bcd
naman8989 <108067200+naman8989@users.noreply.github.com>
2023-03-14 17:11:32 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START IN BUILD_FILE"
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic -noinput --clear
+python3.9 manage.py runserver
 echo "BUILD END IN BUILD_FILE"
